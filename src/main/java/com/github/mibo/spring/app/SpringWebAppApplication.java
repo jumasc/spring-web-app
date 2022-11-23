f8c96a360d33fd16a064309f54f42c44b2aa5fc3
Jürgen Schneider <70094098+jumasc@users.noreply.github.com>
2022-11-23 20:00:00 +0100

    Update SpringWebAppApplication.java

--- src/main/java/com/github/mibo/spring/app/SpringWebAppApplication.java
+++ src/main/java/com/github/mibo/spring/app/SpringWebAppApplication.java
@@ -3,6 +3,7 @@ package com.github.mibo.spring.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringWebAppApplication {
 
